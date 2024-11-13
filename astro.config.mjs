a7b7675ee6edbfcb055820257aa66d8a6045b31c
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-11-13 16:08:57 +0700

    deployer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
-  output: "static",
+  output: "server",
+  adapter: netlify(),
 });
